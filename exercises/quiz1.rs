0b30afa4897f3269eacdd8b09c42006edc781b17
J-shiro <541737231@qq.com>
2022-11-05 13:57:36 +0800

    18/94

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,16 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(x:i32) -> i32 {
+    if x <= 40 {
+        return x*2;
+    }else{
+        return x*1;
+    }
+}
 
 // Don't modify this function!
 #[test]
