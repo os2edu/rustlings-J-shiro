a9c7b577269592ef542f19a776ec41b256f023b1
J-shiro <541737231@qq.com>
2022-11-07 19:39:01 +0800

    39/94

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -4,7 +4,7 @@
 // Make the code compile and the tests pass!
 // Execute `rustlings hint structs3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 #[derive(Debug)]
 struct Package {
@@ -26,12 +26,19 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self) -> bool {
         // Something goes here...
+        if self.sender_country != self.recipient_country{
+            return true;
+        }else{
+            return false;
+        }
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
         // Something goes here...
+        let mut x:i32 = self.weight_in_grams * cents_per_gram;
+        return x;
     }
 }
 
